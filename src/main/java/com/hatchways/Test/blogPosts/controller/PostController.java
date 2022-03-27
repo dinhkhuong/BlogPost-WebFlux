572ccd02606ec4ff0f47fb11abc67beffa7493db
dinhkhuong <lieukhuong@gmail.com>
2022-03-27 16:23:27 -0400

    retrieve from 2 tags parameter

--- src/main/java/com/hatchways/Test/blogPosts/controller/PostController.java
+++ src/main/java/com/hatchways/Test/blogPosts/controller/PostController.java
@@ -2,23 +2,44 @@ package com.hatchways.Test.blogPosts.controller;
 
 import com.hatchways.Test.blogPosts.entities.Posts;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class PostController {
     @Autowired
     private WebClient.Builder webCB;
 
-    @PostMapping(path = "tech")
-    public Posts insertData(){
-        return webCB.build()
+    List<String> tag = Arrays.asList("tech","health");
+
+    @GetMapping(path = "tech")
+    public Flux<Posts> insertData(){
+        /*return webCB.build()
             .get()
             .uri("https://api.hatchways.io/assessment/blog/posts?tag=tech")
             .retrieve()
             .bodyToMono(Posts.class)
-            .block();
+            .block();*/
+
+        Flux<Posts> posts = Flux.fromIterable(tag)
+                .flatMap(x->{
+                    Mono<Posts> post =
+                            webCB.build()
+                            .get()
+                            .uri("https://api.hatchways.io/assessment/blog/posts?tag={tag}",x)
+                            .retrieve()
+                            .bodyToMono(Posts.class);
+                    return post;
+                });
+        return posts;
     }
 }
