55158d9f06b04ef8307b815987ab4fd444cf3362
dinhkhuong <lieukhuong@gmail.com>
2022-04-01 01:08:52 -0400

    retrieve from 2 tags parameter (2)

--- src/main/java/com/hatchways/Test/blogPosts/controller/PostController.java
+++ src/main/java/com/hatchways/Test/blogPosts/controller/PostController.java
@@ -1,18 +1,18 @@
 package com.hatchways.Test.blogPosts.controller;
 
+import com.hatchways.Test.blogPosts.entities.Post;
 import com.hatchways.Test.blogPosts.entities.Posts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @RestController
 public class PostController {
@@ -21,25 +21,31 @@ public class PostController {
 
     List<String> tag = Arrays.asList("tech","health");
 
+    Posts po = new Posts();
     @GetMapping(path = "tech")
-    public Flux<Posts> insertData(){
-        /*return webCB.build()
-            .get()
-            .uri("https://api.hatchways.io/assessment/blog/posts?tag=tech")
-            .retrieve()
-            .bodyToMono(Posts.class)
-            .block();*/
+    public Mono<Posts> insertData(){
 
-        Flux<Posts> posts = Flux.fromIterable(tag)
+        Flux<Post> posts = Flux.fromIterable(tag)
                 .flatMap(x->{
-                    Mono<Posts> post =
+                    List<Post> post =
                             webCB.build()
                             .get()
                             .uri("https://api.hatchways.io/assessment/blog/posts?tag={tag}",x)
                             .retrieve()
-                            .bodyToMono(Posts.class);
-                    return post;
-                });
-        return posts;
+                            .bodyToMono(Posts.class)
+                            .block()
+                            .getPost();
+
+                    return Flux.fromIterable(post);
+
+                })
+                //remove duplicates
+                .distinct(Post::getID);
+        List toPost = StreamSupport
+                .stream(posts.toIterable().spliterator(), false)
+                .collect(Collectors.toList());
+        po.setPost(toPost);
+        return Mono.just(po);
+
     }
 }
